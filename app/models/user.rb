6832ad7e67dd918f69eb3f1b6d73c8a4f2c805c2
rom1026 <rom@icloud.com>
2021-02-08 22:53:26 +0900

    ユーザー管理機能（３）＋α

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,12 +5,12 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
         
   validates :nickname, presence: true
-  validates :password, presence: true, length: { minimum: 6 }, format: { with: /^[0-9a-zA-Z]*$/, message: '半角英数字で入力してください'  }, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: '英数字混合にしてください'  }
-  validates :email, presence: true, format: { with: /@+/, message: '@を使ったアドレスで入力してください' }
-  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: '全角で入力してください' }
-  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: '全角で入力してください' }
-  validates :katakana_last_name, presence: true, format: { with: /\A[\p{katakana}\u{30fc}]+\z/, message: '全角カタカナで入力してください'  }
-  validates :katakana_first_name, presence: true, format: { with: /\A[\p{katakana}\u{30fc}]+\z/, message: '全角カタカナで入力してください'  }
+  validates :password, length: { minimum: 6, message: "パスワードは６文字以上で入力してください" }, format: { with: /^[0-9a-zA-Z]*$/, message: 'パスワードは半角英数字で入力してください'  }, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: 'パスワードは英数字混合にしてください'  }
+  validates :email, format: { with: /@+/, message: '@を使ったアドレスで入力してください' }
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'お名前（全角）は全角で入力してください' }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'お名前（全角）は全角で入力してください' }
+  validates :katakana_last_name, presence: true, format: { with: /\A[\p{katakana}\u{30fc}]+\z/, message: 'お名前カナ（全角）は全角カタカナで入力してください'  }
+  validates :katakana_first_name, presence: true, format: { with: /\A[\p{katakana}\u{30fc}]+\z/, message: 'お名前カナ（全角）は全角カタカナで入力してください'  }
   validates :birth_date, presence: true
 
 
