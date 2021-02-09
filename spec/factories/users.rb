0114c6f0eb67a25a56eb145453138883cc5554e0
rom1026 <rom@icloud.com>
2021-02-09 13:16:50 +0900

    ユーザー管理機能（７）

---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    nickname              {'takumi'}
+    email                 {'test@gmail.com'}
+    password              {'shimano1026'}
+    password_confirmation {password}
+    last_name             {'島野'}
+    first_name            {'工'}
+    katakana_last_name    {'シマノ'}
+    katakana_first_name   {'タクミ'}
+    birth_date            {'1996-10-26'}
+  end
+end
