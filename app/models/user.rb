1462c303ee717cd235c97cd9ef0c5daef99c3e09
rom1026 <rom@icloud.com>
2021-02-09 11:27:54 +0900

    ユーザー管理機能（６）

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,12 +13,15 @@ class User < ApplicationRecord
     validates :katakana_first_name
     validates :birth_date
   end
+
+  ZENKAKU_NAME = /\A[ぁ-んァ-ヶ一-龥々]+\z/
+  ZENKAKU_KANA = /\A[\p{katakana}\u{30fc}]+\z/
       
   validates :password, length: { minimum: 6, message: "パスワードは６文字以上で入力してください" }, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: 'パスワードは英数字混合にしてください'  }
-  validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'お名前（全角）は全角で入力してください' }
-  validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'お名前（全角）は全角で入力してください' }
-  validates :katakana_last_name, format: { with: /\A[\p{katakana}\u{30fc}]+\z/, message: 'お名前カナ（全角）は全角カタカナで入力してください'  }
-  validates :katakana_first_name, format: { with: /\A[\p{katakana}\u{30fc}]+\z/, message: 'お名前カナ（全角）は全角カタカナで入力してください'  }
+  validates :last_name, format: { with: ZENKAKU_NAME, message: 'お名前（全角）は全角で入力してください' }
+  validates :first_name, format: { with: ZENKAKU_NAME, message: 'お名前（全角）は全角で入力してください' }
+  validates :katakana_last_name, format: { with: ZENKAKU_KANA, message: 'お名前カナ（全角）は全角カタカナで入力してください'  }
+  validates :katakana_first_name, format: { with: ZENKAKU_KANA, message: 'お名前カナ（全角）は全角カタカナで入力してください'  }
 
 
 end
