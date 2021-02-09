4f11a393b6865af728177be82dbd62d924beb69c
rom1026 <rom@icloud.com>
2021-02-09 15:42:16 +0900

    商品テーブル作成

--- app/models/items.rb
+++ app/models/items.rb
@@ -1,4 +1,10 @@
-class Address < ApplicationRecord
+class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
+
+  belongs_to :user
+  has_one :purchase
+
+
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_05_090057) do
+ActiveRecord::Schema.define(version: 2021_02_09_053949) do
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.integer "prefecture_id"
-    t.string "city"
+    t.string "product_name", default: "", null: false
+    t.text "product_details", null: false
+    t.integer "category_id", null: false
+    t.integer "product_status_id", null: false
+    t.integer "shipping_charges_id", null: false
+    t.integer "prefecture_id", null: false
+    t.integer "days_to_ship_id", null: false
+    t.integer "selling_price", null: false
+    t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -37,4 +45,5 @@ ActiveRecord::Schema.define(version: 2021_02_05_090057) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "items", "users"
 end
