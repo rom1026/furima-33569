43ec2ebc85e06e949b2b05dee7b043de7492d0e3
rom1026 <rom@icloud.com>
2021-02-09 10:56:07 +0900

    ユーザー管理機能（５）

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,7 +15,6 @@ class User < ApplicationRecord
   end
       
   validates :password, length: { minimum: 6, message: "パスワードは６文字以上で入力してください" }, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: 'パスワードは英数字混合にしてください'  }
-  validates :email, format: { with: /@+/, message: '@を使ったアドレスで入力してください' }
   validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'お名前（全角）は全角で入力してください' }
   validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'お名前（全角）は全角で入力してください' }
   validates :katakana_last_name, format: { with: /\A[\p{katakana}\u{30fc}]+\z/, message: 'お名前カナ（全角）は全角カタカナで入力してください'  }
