5ebe030d281b7f0eb389b93db0f689911a5be912
rom1026 <rom@icloud.com>
2021-02-10 15:47:38 +0900

    商品出品機能（２）

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -36,30 +36,59 @@ RSpec.describe Item, type: :model do
       @item.valid?
       expect(@item.errors.full_messages).to include("Category can't be blank")    
     end
+    it "categoryが0以外でないと登録できないこと" do
+      @item.category_id = 0
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Category must be other than 0")    
+    end
 
     it "product_statusが空では登録できない" do
       @item.product_status_id = ""
       @item.valid?
       expect(@item.errors.full_messages).to include("Product status can't be blank")    
     end
+    it "product_statusが0以外でないと登録できないこと" do
+      @item.product_status_id = 0
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Product status must be other than 0")    
+    end
+
 
     it "shipping_chargeが空では登録できない" do
       @item.shipping_charge_id = ""
       @item.valid?
       expect(@item.errors.full_messages).to include("Shipping charge can't be blank")    
     end
+    it "shipping_chargeが0以外でないと登録できないこと" do
+      @item.shipping_charge_id = 0
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Shipping charge must be other than 0")    
+    end
+
 
     it "prefectureが空では登録できない" do
       @item.prefecture_id = ""
       @item.valid?
       expect(@item.errors.full_messages).to include("Prefecture can't be blank")    
     end
+    it "prefectureが0以外でないと登録できないこと" do
+      @item.prefecture_id = 0
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Prefecture must be other than 0")    
+    end
+
 
     it "days_to_shipが空では登録できない" do
       @item.days_to_ship_id = ""
       @item.valid?
       expect(@item.errors.full_messages).to include("Days to ship can't be blank")    
     end
+    it "days_to_shipが0以外でないと登録できないこと" do
+      @item.days_to_ship_id = 0
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Days to ship must be other than 0")    
+    end
+
 
     it "selling_priceが空では登録できない" do
       @item.selling_price = ""
@@ -71,6 +100,24 @@ RSpec.describe Item, type: :model do
       @item.user = nil
       @item.valid?
       expect(@item.errors.full_messages).to include("User must exist")    
+    end
+
+    it "selling_priceが299円以下では登録できない" do
+      @item.selling_price = 200
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Selling price is not included in the list")    
+    end
+
+    it "selling_priceが10,000,000以上では登録できない" do
+      @item.selling_price = 10000001
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Selling price is not included in the list")    
+    end
+
+    it "selling_priceが半角数字でないと登録できないこと" do
+      @item.selling_price = "１００"
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Selling price is not included in the list")    
     end
 
   end
