65b70bb7d29328e9b4831f3d276847a84a0b7aa9
rom1026 <rom@icloud.com>
2021-02-16 16:52:50 +0900

    商品購入機能（２）

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,5 @@
 class OrdersController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :create]
+  before_action :authenticate_user!, only: [:index, :create]
   before_action :set_item, only: [:index, :create]
   before_action :check_buy, only: [:index, :create]
 
