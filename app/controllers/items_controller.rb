d226c69a67d6e7ec40fb46c4644006f3acb1e307
rom1026 <rom@icloud.com>
2021-02-17 20:36:42 +0900

    [authenticate_user!]にeditを追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!, except: [:index, :show, :edit]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
   before_action :check_user, only: [:edit, :update, :destroy]
   before_action :check_edit, only: [:edit, :update, :destroy]
